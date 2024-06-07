8cf8446697b4d349028da62b655674504c5b2924
LuffysMan <924925465@qq.com>
2024-06-07 08:09:21 +0800

    项目13: 水果商店优化1

---
+++ pro13-fruit1.8-mvc/src/com/atguigu/fruit/servlets/FruitServlet.java
@@ -0,0 +1,153 @@
+package com.atguigu.fruit.servlets;
+
+import com.atguigu.fruit.dao.FruitDAO;
+import com.atguigu.fruit.dao.impl.FruitDAOImpl;
+import com.atguigu.fruit.pojo.Fruit;
+import com.atguigu.myssm.myspringmvc.ViewBaseServlet;
+import com.atguigu.myssm.util.StringUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+import static com.atguigu.myssm.util.StringUtil.isNotEmpty;
+
+@WebServlet("/fruit.do")
+public class FruitServlet extends ViewBaseServlet {
+    private final FruitDAO fruitDAO = new FruitDAOImpl();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String operate = req.getParameter("operate");
+        if (StringUtil.isEmpty(operate)) {
+            operate = "index";
+        }
+        switch (operate) {
+            case "index" -> index(req, resp);
+            case "add" -> add(req, resp);
+            case "del" -> del(req, resp);
+            case "edit" -> edit(req, resp);
+            case "update" -> update(req, resp);
+            default -> throw new RuntimeException("operate值非法!");
+        }
+    }
+
+    private void update(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1. 设置编码
+        req.setCharacterEncoding("utf-8");
+
+        // 2. 设置参数
+        int fid = Integer.parseInt(req.getParameter("fid"));
+        String fname = req.getParameter("fname");
+        int price = Integer.parseInt(req.getParameter("price"));
+        int fcount = Integer.parseInt(req.getParameter("fcount"));
+        String remark = req.getParameter("remark");
+
+        // 3. 执行更新
+        fruitDAO.updateFruit(new Fruit(fid, fname, price, fcount, remark));
+
+        // 4. 资源跳转
+        // super.processTemplate("index", req, resp);
+        // req.getRequestDispatcher("index.html").forward(req, resp);
+        // 此处需要重定向, 目的是重定向给IndexServlet发请求, 重新获取fruitList, 然后覆盖到session中, 这样
+        // index.html页面上显示的数据才是最新的.
+        resp.sendRedirect("fruit.do");
+    }
+
+    private void edit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String fidStr = req.getParameter("fid");
+        if (isNotEmpty(fidStr)) {
+            int fid = Integer.parseInt(fidStr);
+            Fruit fruit = fruitDAO.getFruitByFid(fid);
+            req.setAttribute("fruit", fruit);
+            super.processTemplate("edit", req, resp);
+        }
+    }
+
+    private void index(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int pageNo = 1;
+
+        String pageNoStr = req.getParameter("pageNo");
+        if (StringUtil.isNotEmpty(pageNoStr)) {
+            pageNo = Integer.parseInt(pageNoStr);
+        }
+
+        //保存到session作用域
+        HttpSession session = req.getSession();
+        session.setAttribute("pageNo", pageNo);
+
+        //如果oper!=null 说明 通过表单的查询按钮点击过来的
+        //如果oper==null 说明 不是通过表单的查询按钮点击过来的
+        String oper = req.getParameter("oper");
+
+        String keyword = null;
+        if (StringUtil.isNotEmpty(oper) && "search".equals(oper)) {
+            //说明是点击表达查询发送过来的请求
+            //此时, pageNo应该还原为1, keyword应该从请求参数中获取
+            pageNo = 1;
+            // 如果keyword为null, 需要设置为空字符串"", 否则查询sql会拼接成%null%, 我们期望的%%
+            keyword = req.getParameter("keyword");
+            if (StringUtil.isEmpty(keyword)) {
+                keyword = "";
+            }
+            // 如果不是点击的查询按钮, 那么查询是基于session中保存的现有keyword进行查询
+            session.setAttribute("keyword", keyword);
+        } else {
+            //说明此处不是点击表单查询发送过来的请求(比如点击上一页下一页, 或者直接发送请求)
+            //此时keyword应该从session作用域获取
+            Object keywordObj = session.getAttribute("keyword");
+            if (keywordObj != null) {
+                keyword = (String) keywordObj;
+            } else {
+                keyword = "";
+            }
+
+        }
+
+        FruitDAO fruitDAO = new FruitDAOImpl();
+        List<Fruit> fruitList = fruitDAO.getFruitList(keyword, pageNo);
+        session.setAttribute("fruitList", fruitList);
+
+        //总记录条数
+        int fruitCount = fruitDAO.getFruitCount(keyword);
+        //总页数
+        int pageCount = (fruitCount + 5 - 1) / 5;
+
+        session.setAttribute("pageCount", pageCount);
+
+        // 此处的视图名称是 index
+        // 那么thymeleaf会将这个 逻辑视图名称 对应到 物理视图 名称上去
+        // 逻辑视图名称: index
+        // 物理视图名称: view-prefix + 逻辑视图名称 + view-suffix
+        // 所以真实的视图名称: / + index + .html
+        super.processTemplate("index", req, resp);
+    }
+
+    private void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String fname = req.getParameter("fname");
+        int price = Integer.parseInt(req.getParameter("price"));
+        int fcount = Integer.parseInt(req.getParameter("fcount"));
+        String remark = req.getParameter("remark");
+
+        Fruit fruit = new Fruit(0, fname, price, fcount, remark);
+        fruitDAO.addFruit(fruit);
+
+        resp.sendRedirect("fruit.do");
+    }
+
+    private void del(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String fidStr = req.getParameter("fid");
+        if (StringUtil.isNotEmpty(fidStr)) {
+            int fid = Integer.parseInt(fidStr);
+            fruitDAO.delFruit(fid);
+            resp.sendRedirect("fruit.do");
+        }
+
+    }
+}
