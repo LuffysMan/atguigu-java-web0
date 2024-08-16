0a46857e574a87fd0f1514f09e2f99e0c2008ab6
LuffysMan <924925465@qq.com>
2024-08-16 08:08:10 +0800

    项目16: 水果商店优化: 使用controller

---
+++ pro16-fruit2.0-mvc-controller/readme.md
@@ -0,0 +1,102 @@
+review:
+# 1. 设置编码
+1. post提交方式下的设置编码, 防止中文乱码
+   1. request.setCharacterEncoding("utf-8")
+2. get提交方式, tomcat8开始, 编码不需要设置
+   ```
+   // tomcat8之前, get方式设置比较麻烦
+   String fname = request.getParameter("fname");
+   byte[] bytes = fname.getBytes("iso-8859-1");
+   fname = new String(bytes, "UTF-8");
+   ```
+
+# 2. Servlet继承关系以及生命周期
+1. Servlet接口: init(), service(), destroy()
+   2. GenericServlet抽象子类: 实现了init()和destroy(), 留下了abstract service()
+   3. HttpServlet抽象子类: 实现了service方法, 在service方法内部通过request.getMethod()来判断请求的方式, 然后根据请求的方式去调用内部的do方法, 每一个do方法进行了简单实现, 主要是如果请求方式不符合, 则报405错误.目的是让我们的Servlet子类去重写对应的方法(如果重写的不对, 则使用父类的405错误实现)
+2. 生命周期: 实例化, 初始化, 服务, 销毁
+   1. Tomcat负责维护Servlet实例的生命周期
+   2. 每个Servlet在Tomcat容器中只有一个实例, 它是线程不安全的
+   3. Servlet的启动时机: <load-on-startup>
+   4. Servlet3.0开始支持注解: @WebServlet
+
+# 3. HTTP协议
+1. 由Request和Response两部分组成.
+2. 请求包含了三部分
+   1. 请求行
+   2. 请求消息头: 浏览器版本信息等
+   3. 请求主体: 
+      1. GET: query string
+      2. POST: form data/request payload
+3. 响应包含了三部分: 响应行, 响应消息头, 响应主体
+
+# 4. HttpSession
+1. HttpSession: 表示 会话
+2. 为什么需要HttpSession, 原因是因为Http协议是无状态的
+   1. session id
+   2. cookie
+
+# 5. 会话
+## Session 保存作用域
+- 一次会话有效
+- session保存作用域是和具体的某一个session对应的
+- 常用API
+  - void session.setAttribute(k, v)
+  - object session.getAttribute(k)
+- 其他API
+  - session.getId()
+  - session.isNew()
+  - session.getCreationTime()
+  - session.invalidate()
+
+# 6. 服务器内部转发以及客户端重定向
+1. 服务器内部转发: request.getRequestDispatcher("index.html").forward(req, resp)
+   - 一次请求响应的过程, 客户端不感知内部经过了多少次转发
+   - 地址栏无变化
+2. 客户端重定向: response.sendRedirect("index.html")
+   - 两次请求响应的过程. 客户端肯定知道请求URL有变化
+   - 地址栏有变化
+
+# 7. Thymeleaf - 视图模板技术
+1. 添加thymeleaf 的jar包
+2. 新建一个Servlet类:ViewBaseServlet. 目的是为了它的两个方法: 创建模板引擎, 处理模板
+3. 在web.xml文件中添加配置<context-param>
+     1. 配置前缀 view-prifix
+     2. 配置后缀 view-suffix
+     3. xx
+4. 使得我们的Servlet继承ViewBaseServlet
+5. 根据逻辑视图名称 得到 物理视图名称
+6. 使用thymeleaf的标签
+   1. th:if
+   2. th:unless
+   3. th:each
+   4. th:text
+
+# 今日课程: 保存作用域
+原始情况下, 保存作用域我们可以认为有四个
+1. page(页面级别, 现在几乎不用)
+2. request(一次请求响应范围)
+   1. demo01(servlet1): request.setAttribute(k, v) + response.sendRedirect("demo02) 
+   2. demo02(servlet2): v = request.getAttribute(k), System.out.println(v);
+3. session(一次会话范围)
+4. application(一次应用程序范围有效)
+
+# 路径问题
+## 相对路径
+
+## 绝对路径
+
+# 实现库存系统的功能
+之前的实现, 当业务越来越多, servlet的数量会爆炸
+
+# FAQ
+## 使用ant compile 传递任意参数
+在javac任务中, 增加compilearg标签即可
+```xml
+<javac source="1.8" target="1.8" debug="true" srcdir="src" destdir="build/classes">
+    <classpath refid="classpath" />
+    <compilerarg value="-parameters" />
+</javac>
+```
+
+## 
---
+++ pro16-fruit2.0-mvc-controller/src/com/atguigu/myssm/myspringmvc/DispatcherServlet.java
@@ -0,0 +1,137 @@
+package com.atguigu.myssm.myspringmvc;
+
+import com.atguigu.myssm.util.StringUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("*.do")
+public class DispatcherServlet extends ViewBaseServlet {
+    private final Map<String, Object> beanMap = new HashMap<>();
+
+    public DispatcherServlet() {
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("applicationContext.xml");
+        //1. create DocumentBuilderFactory instance
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        try {
+            //2. create DocumentBuilder instance
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            //3. create Document instance
+            Document document = documentBuilder.parse(inputStream);
+            //4. get all nodes of tag id of "bean"
+            NodeList beanNodeList = document.getElementsByTagName("bean");
+            for (int i = 0; i < beanNodeList.getLength(); i++) {
+                Node beanNode = beanNodeList.item(i);
+                if (beanNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element beanElement = (Element) beanNode;
+                    String beanId = beanElement.getAttribute("id");
+                    String className = beanElement.getAttribute("class");
+                    Class<?> beanClass = Class.forName(className);
+                    Object beanObj = beanClass.getDeclaredConstructor().newInstance();
+                    beanMap.put(beanId, beanObj);
+                }
+            }
+        } catch (ParserConfigurationException | ClassNotFoundException | IOException | SAXException | InvocationTargetException | InstantiationException | IllegalAccessException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 设置编码
+        req.setCharacterEncoding("UTF-8");
+        // 假设url是: http://localhost:8080/pro15/hello.do
+        // 那么servletPath是: /hello.do
+        // 我的思路是:
+        // 第一步: /hello.do -> hello
+        // -> HelloController
+        String servletPath = req.getServletPath();
+        servletPath = servletPath.substring(1);
+        int lastDotIndex = servletPath.lastIndexOf(".do");
+        servletPath = servletPath.substring(0, lastDotIndex);
+
+        Object controllerBeanObj = beanMap.get(servletPath);
+        if (controllerBeanObj == null) {
+            System.out.println("servletPath not found: " + servletPath);
+            throw new RuntimeException("illegal servlet path!");
+        }
+
+        String operate = req.getParameter("operate");
+        if (StringUtil.isEmpty(operate)) {
+            operate = "index";
+        }
+
+        try {
+            Method[] methods = controllerBeanObj.getClass().getDeclaredMethods();
+            for (Method method : methods) {
+                if (operate.equals(method.getName())) {
+                    // 1. 统一获取请求参数
+                    // get parameter array of current method
+                    Parameter[] parameters = method.getParameters();
+                    Object[] parameterValues = new Object[parameters.length];
+                    for (int i = 0; i < parameters.length; i++) {
+                        Parameter parameter = parameters[i];
+                        String parameterName = parameter.getName();
+                        if ("req".equals(parameterName)) {
+                            parameterValues[i] = req;
+                        } else if ("resp".equals(parameterName)) {
+                            parameterValues[i] = resp;
+                        } else if ("session".equals(parameterName)) {
+                            parameterValues[i] = req.getSession();
+                        } else {
+                            String parameterValue = req.getParameter(parameterName);
+                            String typeName = parameter.getType().getName();
+
+                            Object parameterObj = parameterValue;
+                            if (parameterObj != null) {
+                                if ("java.lang.Integer".equals(typeName)) {
+                                    parameterObj = Integer.parseInt(parameterValue);
+                                }
+                            }
+                            parameterValues[i] = parameterObj;
+                        }
+                    }
+
+                    // 2. invoke method of controllers
+                    method.setAccessible(true);
+                    Object returnObj = method.invoke(controllerBeanObj, parameterValues);
+                    // 3. process view
+                    String methodReturnStr = (String) returnObj;
+                    if (methodReturnStr.startsWith("redirect:")) {
+                        String redirectStr = methodReturnStr.substring("redirect:".length());
+                        resp.sendRedirect(redirectStr);
+                    } else {
+                        super.processTemplate(methodReturnStr, req, resp);
+                    }
+                }
+            }
+//            else {
+//                throw new RuntimeException("operate值非法!");
+//            }
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
